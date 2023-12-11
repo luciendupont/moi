ad48e934fd7601940a4f9b336ca823715e62a92e
luciendupont <lucienluludupont@gmail.com>
2023-12-11 16:00:39 +0100

    123

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NativeBaseProvider,Box,Button,Text,Pressable,Menu,HamburgerIcon} from 'native-base';
 
-export default function App() {
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+<Text></Text>
+<Box h="80%" w="90%" alignItems="flex-center">
+      <Menu shadow={2} w="190" trigger={triggerProps => {
+      return <Pressable accessibilityLabel="More options menu" {...triggerProps}>
+              <HamburgerIcon />
+            </Pressable>;
+    }}>
+        <Menu.Item>Arial</Menu.Item>
+        <Menu.Item>Nunito Sans</Menu.Item>
+        <Menu.Item>Roboto</Menu.Item>
+        <Menu.Item>Poppins</Menu.Item>
+        <Menu.Item>SF Pro</Menu.Item>
+        <Menu.Item>Helvetica</Menu.Item>
+        <Menu.Item isDisabled>Sofia</Menu.Item>
+        <Menu.Item>Cookie</Menu.Item>
+      </Menu>
+    </Box>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
